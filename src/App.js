83ace4498d228b0ddf4c1aa3e6f225ba10207461
ricardoairesm <ricardoaamb@usp.br>
2022-11-27 10:41:29 -0300

    css pronto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+    return (
+        <>
+            <div className="jogo">
+                <img src="assets/forca0.png" />
+                <button className="escolherJogo">Escolher Palavra</button>
+                <div className="alfabeto">
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                    <div className="caixinhaLetras">A</div>
+                </div>
+                <input></input>
+                <div className="texto">Já sei a palavra!</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            </div>
+        </>
+    )
 }
-
-export default App;
